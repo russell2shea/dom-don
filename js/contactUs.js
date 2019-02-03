853c80cf55bb275030154454b47219ce58a3af89
Russell Shea <russell2shea@gmail.com>
2019-02-03 12:38:33 -0500

    testing contact form

--- js/contactUs.js
+++ js/contactUs.js
@@ -1,7 +1,52 @@
-// Add an event listener to the submit button
-document.getElementById("contact_form-submit").addEventListener("click", validateForm);
+// On Submit to let required do the validation
+$( ".contact_form" ).submit(function( event ) {
 
-// Check if the form is valid
-function validateForm(){
-    console.log('hey');
-}
+  	// Prevent page reload 
+  	event.preventDefault();
+
+	// get values from FORM
+	var name = $("input[name='name']").val();
+	var email = $("input[name='email']").val();
+	var phone = $("input[name='telephone']").val();
+	var message = $("textarea[name='message']").val();
+
+	console.log(name + " " + email + " " + phone + " " + message);
+
+    $.ajax({
+        url: "https://sites.agorafinancial.com/promos/test/contactUs.php",
+        type: "POST",
+        data: {
+            name: name,
+            email: email,
+            phone: phone,
+            message: message
+        },
+        cache: false,
+        success: function() {
+
+        	alert('Made it');
+
+        },
+        error: function() {
+
+        	alert('Fail');
+
+        },
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
